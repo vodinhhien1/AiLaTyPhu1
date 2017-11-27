c30781dc1a0701b7a427c1a0eeb7dfdf2d3491a4
trigithu <32445937+trigithu@users.noreply.github.com>
2017-11-27 20:22:24 +0700

    Add files via upload

--- QuanLiData.java
+++ QuanLiData.java
@@ -20,8 +20,7 @@ import java.io.InputStream;
 import java.io.IOException;
 
 
-public class QuanLiData extends SQLiteOpenHelper{
-    private static final String TAG = "QuanLiData";
+public class QuanLiData{
     private static final String DATA_NAME = "quanli_cauhoi.sqlite";
     public static final String PATH= Environment.getDataDirectory()+"/data/com.example.ductri.ailatyphu/databases/"+DATA_NAME;
     private static final String TABLE_NAME = "question";
@@ -32,20 +31,10 @@ public class QuanLiData extends SQLiteOpenHelper{
     private static final String CAUC = "cauc";
     private static final String CAUD = "caud";
     private static final String DAPAN = "caudung";
-    private static final int VERSION = 1;
     private Context context;
     private SQLiteDatabase database;
 
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        Log.d(TAG, "onCreate: ");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        Log.d(TAG, "onUpgrade: ");
-    }
     public QuanLiData(Context context) {
-        super(context,DATA_NAME ,null, VERSION);
         this.context = context;
         copyDataBase(context);
     }
@@ -78,27 +67,32 @@ public class QuanLiData extends SQLiteOpenHelper{
     public void closeDaTaBase(){
         database.close();
     }
-    
+
     public ArrayList<Cauhoi> getData(){
         openDataBase();
         ArrayList<Cauhoi> arrQuestions=new ArrayList<>();
             String table = TABLE_NAME;
+        for(int i = 1;i<16;i++){
             String sql="SELECT * FROM "+table+" ORDER BY random() limit 1";
             Cursor cursor= database.rawQuery(sql,null);
-        if(cursor.moveToFirst()){
-            do{
-                Cauhoi cauhoi = new Cauhoi();
-                cauhoi.setId(cursor.getInt(0));
-                cauhoi.setCauhoi(cursor.getString(1));
-                cauhoi.setCauA(cursor.getString(2));
-                cauhoi.setCauB(cursor.getString(3));
-                cauhoi.setCauC(cursor.getString(4));
-                cauhoi.setCauD(cursor.getString(5));
-                cauhoi.setDapan(cursor.getInt(6));
-                arrQuestions.add(cauhoi);
+                int indexId = cursor.getColumnIndex(ID);
+                int indexQUESTION= cursor.getColumnIndex(QUESTION);
+                int indexCaseA=cursor.getColumnIndex(CAUA);
+                int indexCaseB=cursor.getColumnIndex(CAUB);
+                int indexCaseC=cursor.getColumnIndex(CAUC);
+                int indexCaseD=cursor.getColumnIndex(CAUD);
+                int indexTrueCase=cursor.getColumnIndex(DAPAN);
+                cursor.moveToFirst();
+                int id=cursor.getInt(indexId);
+                String cauhoi = cursor.getString(indexQUESTION);
+                String cauA = cursor.getString(indexCaseA);
+                String cauB = cursor.getString(indexCaseB);
+                String cauC = cursor.getString(indexCaseC);
+                String cauD = cursor.getString(indexCaseD);
+                int dapan = cursor.getInt(indexTrueCase);
+                Cauhoi cauhoi1 = new Cauhoi(id,cauhoi,cauA,cauB,cauC,cauD,dapan);
+                arrQuestions.add(cauhoi1);
             }
-            while (cursor.moveToNext());
-        }
         closeDaTaBase();
         return arrQuestions;
     }
